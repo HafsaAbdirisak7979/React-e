a05c0dab85f24ede9ede1356355b5bd634618947
HafsaAbdirisak7979 <engzukuut7914@gmail.com>
2025-09-27 13:42:31 +0300

    Exercise 3

---
+++ Exercise 3/exercise 3/src/App.jsx
@@ -0,0 +1,38 @@
+
+import UserCard from "./UserCard";
+
+function App() {
+  return (
+    <>
+     <div >
+      <UserCard name="Hafsa Abdirisak " 
+      email="Hafsa@gmail.com"
+       age={25}/>
+    </div>
+
+
+       <div >
+      <UserCard name=" Abdirisak " 
+      email="Abdirisak@gmail.com"
+       age={35}/>
+    </div>
+
+
+ <div >
+      <UserCard name="Maryam Abdirisak " 
+      email="Maryam@gmail.com"
+       age={24}/>
+    </div>
+
+    </>
+   
+
+
+
+  );
+}
+      
+
+export default App;
+
+
---
+++ Exercise 3/exercise 3/src/UserCard.jsx
@@ -0,0 +1,14 @@
+const UserCard =({name,email,age})=>{
+    return(
+        <div>
+            <h2>{name}</h2>
+            <p>{email}</p>
+            <p>{age}</p>
+        </div>
+    )
+}
+
+
+
+
+export default UserCard;
